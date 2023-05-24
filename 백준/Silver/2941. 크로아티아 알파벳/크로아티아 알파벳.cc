c87571a6656d3c63e0372babf692e12b35e37721
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-05-24 19:27:13 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -2,39 +2,25 @@
 
 using namespace std;
 
-string croationAlpa[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-
-int compare(int idx, string input)
-{
-	for(int i = 0; i < 8; i++)
-	{
-		int j = 0;
-		while (j < croationAlpa[i].length())
-		{
-			if (input[idx + j] != croationAlpa[i][j])
-				break;
-			j++;
-		}
-		if (j == croationAlpa[i].length())
-			return (croationAlpa[i].length());
-	}
-	return (0);
-}
+string croation[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
 int main (void)
 {
 	string input;
 	cin >> input;
 
-	int result = 0;
-	for(int i = 0; i < input.length(); i++)
+	int idx;
+	for(int i = 0; i < 8; i++)
 	{
-		int alpaLen = compare(i, input);
-		if (alpaLen != 0)
-			i += alpaLen - 1;
-		result++;
+		while(1)
+		{
+			idx = input.find(croation[i]);
+			if (idx == string::npos)
+				break;
+			input.replace(idx, croation[i].length(), "#");
+		}
 	}
-	cout << result << endl;
+	cout << input.length() << endl;
 
 	return (0);
 }
