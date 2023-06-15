79fdadf1579b07db3f5c5c4362a2d13ea2c63005
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-15 13:02:14 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	while(1)
+	{
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+		
+		if (a > b)
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+	}
+	return (0);
+}
