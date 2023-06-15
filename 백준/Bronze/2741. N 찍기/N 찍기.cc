268329c01947874bbc51b9bf1a4d21f7d90033c8
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-15 12:34:51 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+	for(int i = 1; i <= n; i++)
+		cout << i << "\n";
+	return (0);
+}
