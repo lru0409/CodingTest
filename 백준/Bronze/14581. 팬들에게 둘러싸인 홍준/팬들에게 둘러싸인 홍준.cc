f279fc65d4da13b2ad7cd9027a8b7b0139819e4c
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-16 15:10:09 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	string id;
+	cin >> id;
+
+	cout << ":fan::fan::fan:" << "\n";
+	cout << ":fan::" << id << "::fan:" << "\n";
+	cout << ":fan::fan::fan:" << "\n";
+
+	return (0);
+}
