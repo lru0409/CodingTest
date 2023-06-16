078bef53e2bed7fea3be85cc6eabbf9501d29121
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-16 15:36:11 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	int n;
+	cin >> n;
+
+	if (n == 0)
+		cout << "YONSEI" << endl;
+	else if (n == 1)
+		cout << "Leading the Way to the Future" << endl;
+
+	return (0);
+}
