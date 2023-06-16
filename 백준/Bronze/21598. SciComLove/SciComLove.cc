b9d2183971cfeedb4299944786139b8d3fc45f31
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-16 16:11:30 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+	
+	long long n;
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+		cout << "SciComLove" << endl;
+	
+	return (0);
+}
