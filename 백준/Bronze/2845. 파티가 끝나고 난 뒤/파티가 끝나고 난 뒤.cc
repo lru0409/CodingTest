e9049d9a2b3d2bacc45af14ad2f0c1c602c06e5b
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-22 10:15:22 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+
+	int l, p;
+	cin >> l >> p;
+
+	int tmp;
+	for(int i = 0; i < 5; i++)
+	{
+		cin >> tmp;
+		cout << tmp - (l * p) << " ";
+	}
+
+	return (0);
+}
