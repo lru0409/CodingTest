995524541664c933b4a5a0d4e7f329cc860cd80c
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-27 17:15:13 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+
+	unsigned long long n, m;
+	cin >> n >> m;
+	cout << n * m / 2;
+
+	return (0);
+}
