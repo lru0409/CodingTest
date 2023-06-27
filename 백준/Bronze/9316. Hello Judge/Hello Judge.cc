6e34ff5f64d0dee52d1dbbb7c10ad03a142c5a1c
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-06-27 12:37:20 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+
+	for(int i = 1; i <= n; i++)
+		cout << "Hello World, Judge " << i << "!\n";
+
+	return (0);
+}
