9690f572ce8f4081ec0029b05e6301e45a32f030
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-07-04 18:38:17 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+
+	int	n;
+	cin >> n;
+
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 0; j < n - i; j++)
+			cout << " ";
+		for(int j = 0; j < 2 * i - 1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	
+	return (0);
+}
