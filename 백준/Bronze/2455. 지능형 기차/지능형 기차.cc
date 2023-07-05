ca237177a0ba0e5ac298c70f37d1d0dfd5b3f6a8
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-07-05 11:18:09 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+
+	int max = -1;
+	int	cur = 0;
+	int a, b;
+	for(int i = 0; i < 4; i++)
+	{
+		cin >> a >> b;
+		cur -= a;
+		cur += b;
+		if (cur > max)
+			max = cur;
+	}
+	cout << max;
+	
+	return (0);
+}
