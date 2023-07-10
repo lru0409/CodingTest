ccada575ac6cf2857fae0a62673b98a4f90d0528
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-07-11 08:59:56 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, int> p1, pair<int, int> p2)
+{
+	if (p1.second == p2.second)
+		return (p1.first < p2.first);
+	return (p1.second < p2.second);
+}
+
+int main (void)
+{
+	ios_base::sync_with_stdio(false);
+
+	int	n;
+	cin >> n;
+
+	vector< pair<int, int> > points;
+	for(int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		points.push_back(make_pair(x, y));
+	}
+
+	sort(points.begin(), points.end(), compare);
+
+	for(int i = 0; i < n; i++)
+		cout << points[i].first << " " << points[i].second << "\n";
+
+	return (0);
+}
