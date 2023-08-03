d3c593b322c2c0e40296323a519f43a07cb923f7
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-08-03 15:09:37 +0900

    [Silver V] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4446. ROT13/ROT13.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char A[6] = {'A', 'I', 'Y', 'E', 'O', 'U'};
+char B[20] = {'B', 'K', 'X', 'Z', 'N', 'H', 'D', 'C', 'W', 'G', 'P', 'V', 'J', 'Q', 'T', 'S', 'R', 'L', 'M', 'F'};
+char a[6] = {'a', 'i', 'y', 'e', 'o', 'u'};
+char b[20] = { 'b', 'k', 'x', 'z', 'n', 'h', 'd', 'c', 'w', 'g', 'p', 'v', 'j', 'q', 't', 's', 'r', 'l', 'm', 'f'};
+
+char change(char *a, char *b, char c)
+{
+	for(int i = 0; i < 6; i++)
+	{
+		if (c == a[i])
+		{
+			i += 3;
+			if (i >= 6)
+				return (a[i - 6]);
+			return (a[i]);
+		}
+	}
+	for(int i = 0; i < 20; i++)
+	{
+		if (c == b[i])
+		{
+			i += 10;
+			if (i >= 20)
+				return (b[i - 20]);
+			return (b[i]);
+		}
+	}
+	return (0);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+
+	string str;
+	while (!cin.eof())
+	{
+		getline(cin, str);
+		
+		int idx = 0;
+		int alpha;
+		while (str[idx])
+		{
+			if ('a' <= str[idx] && str[idx] <= 'z')
+				str[idx] = change(a, b, str[idx]);
+			else if ('A' <= str[idx] && str[idx] <= 'Z')
+				str[idx] = change(A, B, str[idx]);
+			idx++;
+		}
+		cout << str << "\n";
+	}
+	
+	return (0);
+}
