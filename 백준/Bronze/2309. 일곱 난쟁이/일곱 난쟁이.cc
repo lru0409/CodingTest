4ceb8c72ecf1c8a5f2e0a8adcd6fb8d2f9a564e5
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-08-09 18:46:25 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	int sum = 0;
+	int height[9];
+
+	for(int i = 0; i < 9; i++)
+	{
+		cin >> height[i];
+		sum += height[i];
+	}
+
+	sort(height, height + 9);
+
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = i+1; j < 9; j++)
+		{
+			if (sum - height[i] - height[j] == 100)
+			{
+				for(int k = 0; k < 9; k++)
+					if (!(k == i) && !(k == j)) cout << height[k] << "\n";
+				return (0);
+			}
+		}
+	}
+	
+	return (0);
+}
