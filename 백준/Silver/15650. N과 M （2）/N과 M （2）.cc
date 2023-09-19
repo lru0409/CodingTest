c0f2dcf1317e970dbe13077fc442e7c5ba3a84ae
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-09-19 11:49:06 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#define MAX 9
+using namespace std;
+
+int N, M;
+int arr[MAX];
+
+void printArr()
+{
+	for(int i = 0; i < M; i++)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+void permutation(int depth, int next)
+{
+	if (depth == M) {
+		printArr();
+		return ;
+	}
+	for(int i = next; i <= N; i++)
+	{
+		arr[depth] = i;
+		permutation(depth + 1, i + 1);
+	}
+}
+
+int main(void)
+{
+	cin >> N >> M;
+	permutation(0, 1);
+
+	return (0);
+}
