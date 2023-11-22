5744495ec4130b959239b4e82b3d5762f121647a
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-11-22 14:16:59 +0900

    [Gold V] Title: 토마토, Time: 308 ms, Memory: 12436 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -3,6 +3,8 @@
 #include <vector>
 using namespace std;
 
+queue<vector<int> > Q;
+
 int M, N, H;
 int tomato[100][100][100];
 int result = 0;
@@ -11,55 +13,25 @@ int dh[6] = { 0, 0, 0, 0, -1, 1 };
 int dy[6] = { -1, 1, 0, 0, 0, 0 };
 int dx[6] = { 0, 0, -1, 1, 0, 0 };
 
-void setQueue(queue<vector<int> >& Q)
-{
-	for(int i = 0; i < H; i++) {
-		for(int j = 0; j < N; j++) {
-			for(int k = 0; k < M; k++) {
-				if (tomato[i][j][k] == 1) // 익은 토마토
-				{
-					vector<int> v;
-					v.push_back(i);
-					v.push_back(j);
-					v.push_back(k);
-					v.push_back(0);
-					Q.push(v);
-				}
-			}
-		}
-	}
-}
 
-// 익은 토마토가 있는 곳을 기준으로 동시에 돌린다?
 void BFS()
 {
-	queue<vector<int> > Q;
-	setQueue(Q);
-
 	while (!Q.empty())
 	{
-		int ch = Q.front()[0];
-		int cy = Q.front()[1];
-		int cx = Q.front()[2];
-		int day = Q.front()[3];
+		vector<int> cur = Q.front();
 		Q.pop();
 
 		for(int i = 0; i < 6; i++)
 		{
-			int nh = ch + dh[i];
-			int ny = cy + dy[i];
-			int nx = cx + dx[i];
+			int nh = cur[0] + dh[i], ny = cur[1] + dy[i], nx = cur[2] + dx[i];
 			if (nh < 0 || ny < 0 || nx < 0 || nh >= H || ny >= N || nx >= M) continue;
 			if (tomato[nh][ny][nx] != 0) continue;
-
 			tomato[nh][ny][nx] = 1;
-			vector<int> v;
-			v.push_back(nh);
-			v.push_back(ny);
-			v.push_back(nx);
-			v.push_back(day + 1);
-			result = day + 1;
-			Q.push(v);
+
+			vector<int> next(4);
+			next[0] = nh, next[1] = ny, next[2] = nx, next[3] = cur[3] + 1;
+			result = cur[3] + 1;
+			Q.push(next);
 		}
 	}
 }
@@ -81,17 +53,34 @@ bool is_unripie_tomato()
 int main()
 {
 	cin >> M >> N >> H;
-	for(int i = 0; i < H; i++)
-		for(int j = 0; j < N; j++)
+	for(int i = 0; i < H; i++) {
+		for(int j = 0; j < N; j++) {
 			for(int k = 0; k < M; k++)
+			{
 				cin >> tomato[i][j][k];
+				if (tomato[i][j][k] == 1)
+				{
+					vector<int> v(4);
+					v[0] = i, v[1] = j, v[2] = k, v[3] = 0;
+					Q.push(v);
+				}
+			}
+		}
+	}
 	
 	BFS();
 
-	if (is_unripie_tomato())
-		cout << -1 << endl;
-	else
-		cout << result << endl;
+	for(int i = 0; i < H; i++) {
+		for(int j = 0; j < N; j++) {
+			for(int k = 0; k < M; k++) {
+				if (tomato[i][j][k] == 0) {
+					cout << -1 << endl;
+					return (0);
+				}
+			}
+		}
+	}
+	cout << result << endl;
 	
 	return (0);
 }
