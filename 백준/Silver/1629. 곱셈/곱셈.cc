ead3973dd18703473847e8f5c989144d031423d2
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-11-28 11:28:48 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+long long A, B, C;
+
+long long power(long long n)
+{
+	if (n == 1)
+		return (A % C);
+	
+	long long ret = power(n/2);
+	if (n % 2 == 0)
+		return (ret * ret % C);
+	else
+		return (ret * ret % C * A % C);
+}
+
+int main()
+{
+	cin >> A >> B >> C;
+	cout << power(B) << '\n';
+
+	return (0);
+}
