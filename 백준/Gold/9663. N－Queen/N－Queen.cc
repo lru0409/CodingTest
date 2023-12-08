1d57bab55700e914ff1dba2ba84477ed0a042be6
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2023-12-08 09:27:39 +0900

    [Gold IV] Title: N-Queen, Time: 4356 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,54 +1,22 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int N;
-int result = 0;
+int queens[15]; // 퀸의 y축 위치
+int N, result = 0;
 
-vector<int> queens; // 퀸의 y축 위치
-
-// 이전 퀸들과 가로로 겹치지 않는가?
-bool isPossibleHorizontal(int count, int y)
+bool check(int x)
 {
-	for(int i = 0; i < count; i++)
+	for(int i = 0; i < x; i++)
 	{
-		if (queens[i] == y)
+		if (queens[i] == queens[x] || abs(queens[x] - queens[i]) == x - i)
 			return (false);
 	}
 	return (true);
 }
 
-// 이전 퀸들과 대각선으로 겹치지 않는가?
-bool isPossibleDiagonal(int count, int y)
+void NQueen(int x)
 {
-	// 왼쪽 위로 가는 대각선
-	int tmp_cnt = count - 1;
-	int tmp_y = y - 1;
-	while (tmp_cnt >= 0 && tmp_y >= 0)
-	{
-		if (queens[tmp_cnt] == tmp_y)
-			return (false);
-		tmp_cnt--;
-		tmp_y--;
-	}
-
-	// 왼쪽 아래로 가는 대각선
-	tmp_cnt = count - 1;
-	tmp_y = y + 1;
-	while (tmp_cnt >= 0 && tmp_y < N)
-	{
-		if (queens[tmp_cnt] == tmp_y)
-			return (false);
-		tmp_cnt--;
-		tmp_y++;
-	}
-
-	return (true);
-}
-
-void NQueen(int count)
-{
-	if (count == N)
+	if (x == N)
 	{
 		result += 1;
 		return;
@@ -56,15 +24,11 @@ void NQueen(int count)
 	
 	for(int i = 0; i < N; i++)
 	{
-		if (isPossibleHorizontal(count, i) && isPossibleDiagonal(count, i))
-		{
-			queens.push_back(i);
-			NQueen(count + 1);
-			queens.pop_back();
-		}
+		queens[x] = i;
+		if (check(x))
+			NQueen(x + 1);
 	}
 }
-
 
 int main()
 {
