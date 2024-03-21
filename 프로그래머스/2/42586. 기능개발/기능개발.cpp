180adf814403190fe5c77107ceab1c0b2ee341b9
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-03-21 16:50:00 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,39 +1,29 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-// 7 3 9
-// 5 10 1 1 20 1
-#include <iostream>
-
 vector<int> solution(vector<int> progresses, vector<int> speeds)
 {
-    vector<int> complete_dates;
+    vector<int> answer;
+    
+    int release_day;
     for(int i = 0; i < progresses.size(); i++)
     {
-        int date = (100 - progresses[i]) / speeds[i];
-        if(date * speeds[i] != 100 - progresses[i])
-            date += 1;
-        complete_dates.push_back(date);
-    }
-    
-    vector<int> answer;
-    int release_date = complete_dates[0];
-    int count = 1;
-    int idx = 1;
-    while(idx < complete_dates.size())
-    {
-        if(release_date < complete_dates[idx])
+        // i번째 작업을 완료하기 위해 필요한 날짜 수 day 구하기
+        int day = (100 - progresses[i]) / speeds[i];
+        if((100 - progresses[i]) % speeds[i] != 0)
+            day += 1;
+        
+        if(i == 0 || release_day < day) // 첫 작업이거나, 배포 날짜까지 완료할 수 없는 작업인 경우
         {
-            answer.push_back(count);
-            release_date = complete_dates[idx];
-            count = 0;
+            answer.push_back(1);
+            release_day = day;
         }
-        count++;
-        idx++;
+        else // 배포 날짜까지 완료할 수 있는 작업인 경우
+        {
+            answer.back() += 1;
+        }
     }
-    answer.push_back(count);
     return answer;
 }
