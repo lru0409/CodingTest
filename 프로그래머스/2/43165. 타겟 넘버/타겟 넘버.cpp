d63ee32b9b9c5a6d4c59859f02746619e4cfdcf1
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-03-27 10:13:31 +0900

    [level 2] Title: 타겟 넘버, Time: 0.04 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,36 +3,23 @@
 using namespace std;
 
 int answer = 0;
-vector<int> perm;
 
-void check(vector<int>& numbers, int target)
+void DFS(int sum, int count, vector<int>& numbers, int target)
 {
-    int result = 0;
-    for(int i = 0; i < numbers.size(); i++)
-        result += numbers[i] * perm[i];
-    if (result == target)
-        answer += 1;
-}
-
-void permutation(vector<int>& numbers, int target)
-{
-    if (perm.size() == numbers.size())
+    if (count == numbers.size())
     {
-        check(numbers, target);
+        if (sum == target)
+            answer += 1;
         return;
     }
-    perm.push_back(1);
-    permutation(numbers, target);
-    perm.pop_back();
-    perm.push_back(-1);
-    permutation(numbers, target);
-    perm.pop_back();
+    DFS(sum + numbers[count], count + 1, numbers, target);
+    DFS(sum - numbers[count], count + 1, numbers, target);
 }
 
 using namespace std;
 
 int solution(vector<int> numbers, int target)
 {
-    permutation(numbers, target);
+    DFS(0, 0, numbers, target);
     return answer;
 }
