ddfd5d90716ad714589ddb2c7f41211296494bbf
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-03-30 13:14:26 +0900

    [level 3] Title: 기둥과 보 설치, Time: 0.43 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.cpp
@@ -0,0 +1,94 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define PILLAR 0
+#define BEAM 1
+
+#define REMOVE 0
+#define INSTALL 1
+
+bool wall[101][101][2];
+
+#include <iostream>
+
+bool check_build(int x, int y, int type, int n) // 1 0 기둥  - 1 1 보 - 2 1 기둥
+{
+    if (type == PILLAR) { // 기둥
+        if (y == 0) return true; // 바닥 위
+        if (wall[x][y][BEAM]) return true; // 보 왼쪽 끝 위
+        if (0 <= x-1 && wall[x-1][y][BEAM]) return true; // 보 오른쪽 끝 위
+        if (0 <= y-1 && wall[x][y-1][PILLAR]) return true; // 기둥 위
+    }
+    else { // 보
+        if (0 <= x-1 && wall[x-1][y][BEAM] && x+1 <= n && wall[x+1][y][BEAM]) return true; // 양쪽 끝 보
+        if (0 <= y-1 && wall[x][y-1][PILLAR]) return true; // 왼쪽 아래 기둥
+        if (0 <= y-1 && x+1 <= n && wall[x+1][y-1][PILLAR]) return true; // 오른쪽 아래 기둥
+    }
+    return false;
+}
+
+bool check_remove(int x, int y, int type, int n)
+{
+    wall[x][y][type] = false;
+    if (type == PILLAR) { // 기둥
+		// 위의 기둥
+		if (y+1 <= n && wall[x][y+1][PILLAR] && !check_build(x, y+1, PILLAR, n)) return false;
+		// 위의 좌우 보
+		if (y+1 <= n && wall[x-1][y+1][BEAM] && !check_build(x-1, y+1, BEAM, n)) return false;
+        if (y+1 <= n && wall[x][y+1][BEAM] && !check_build(x, y+1, BEAM, n)) return false; 
+    }
+    else { // 보
+		// 위의 좌우 기둥
+		if (wall[x][y][PILLAR] && !check_build(x, y, PILLAR, n)) return false;
+		if (x+1 <= n && wall[x+1][y][PILLAR] && !check_build(x+1, y, PILLAR, n)) return false;
+		// 좌우 보
+		if (0 <= x-1 && wall[x-1][y][BEAM] && !check_build(x-1, y, BEAM, n)) return false;
+		if (x+1 <= n && wall[x+1][y][BEAM] && !check_build(x+1, y, BEAM, n)) return false;
+    }
+    return true;
+}
+
+// bool comp(vector<int> v1, vector<int> v2)
+// {
+//     if (v1[0] < v2[0]) return true;
+//     else if (v1[0] > v2[0]) return false;
+    
+//     if (v1[1] < v2[1]) return true;
+//     else if (v1[1] > v2[1]) return false;
+
+//     if (v1[2] <= v2[2]) return true;
+//     else return false;
+// }
+
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame)
+{    
+	for(int i = 0; i < build_frame.size(); i++)
+	{
+        int x = build_frame[i][0];
+        int y = build_frame[i][1];
+        int type = build_frame[i][2];
+        int work = build_frame[i][3];
+        
+        if (work == REMOVE) {	// 제거 작업
+            if (check_remove(x, y, type, n) == false)
+                wall[x][y][type] = true;
+        } else {				// 설치 작업
+            if (check_build(x, y, type, n))
+                wall[x][y][type] = true;
+        }
+    }
+    
+	vector<vector<int>> answer;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+			if (wall[i][j][PILLAR])
+				answer.push_back({i, j, PILLAR});
+			if (wall[i][j][BEAM])
+				answer.push_back({i, j, BEAM});
+        }
+    }
+    
+    return answer;
+}
