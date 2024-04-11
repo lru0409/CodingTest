332f008314ed00036780633c3c0b24967bc70e01
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-04-12 08:20:42 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.25 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -5,40 +5,13 @@
 
 using namespace std;
 
-unordered_map<string, int> gift_index; // 선물 지수
-unordered_map<string, unordered_map<string, int>> give_count; // 각 친구에게 선물 준 횟수
-unordered_map<string, int> receive_count; // 다음 달에 선물 받는 횟수
-
-vector<string> tmp;
-
-void Recursive(vector<string>& friends, int next)
-{
-    if (tmp.size() == 2)
-    {
-        if (give_count[tmp[0]][tmp[1]] < give_count[tmp[1]][tmp[0]])
-            receive_count[tmp[1]] += 1;
-        else if (give_count[tmp[0]][tmp[1]] > give_count[tmp[1]][tmp[0]])
-            receive_count[tmp[0]] += 1;
-        else
-        {
-            if (gift_index[tmp[0]] < gift_index[tmp[1]])
-                receive_count[tmp[1]] += 1;
-            else if (gift_index[tmp[0]] > gift_index[tmp[1]])
-                receive_count[tmp[0]] += 1;
-        }
-        return;
-    }
-    
-    for(int i = next; i < friends.size(); i++)
-    {
-        tmp.push_back(friends[i]);
-        Recursive(friends, i + 1);
-        tmp.pop_back();
-    }
-}
-
 int solution(vector<string> friends, vector<string> gifts)
-{  
+{
+    unordered_map<string, int> gift_index; // 선물 지수
+    unordered_map<string, unordered_map<string, int>> give_count; // 각 친구에게 선물 준 횟수
+    unordered_map<string, int> receive_count; // 다음 달에 선물 받는 횟수
+
+    // 각 친구의 선물 지수, 다른 친구에게 선물 준 횟수 저장하기
     for(int i = 0; i < gifts.size(); i++)
     {
         stringstream ss(gifts[i]);
@@ -50,8 +23,24 @@ int solution(vector<string> friends, vector<string> gifts)
         give_count[given][received] += 1;
     }
     
-    Recursive(friends, 0);
+    // 모든 친구들의 조합에 대해서 누가 선물을 받는지 구하기
+    for(int i = 0; i < friends.size(); i++)
+    {
+        for(int j = i + 1; j < friends.size(); j++)
+        {
+            string f1 = friends[i], f2 = friends[j];
+            if (give_count[f1][f2] < give_count[f2][f1])
+                receive_count[f2] += 1;
+            else if (give_count[f1][f2] > give_count[f2][f1])
+                receive_count[f1] += 1;
+            else if (gift_index[f1] < gift_index[f2])
+                receive_count[f2] += 1;
+            else if (gift_index[f1] > gift_index[f2])
+                receive_count[f1] += 1;
+        }
+    }
     
+    // 가장 큰 선물 받은 횟수를 구하기
     unordered_map<string, int>::iterator it;
     int answer = 0;
     for(it = receive_count.begin(); it != receive_count.end(); it++)
