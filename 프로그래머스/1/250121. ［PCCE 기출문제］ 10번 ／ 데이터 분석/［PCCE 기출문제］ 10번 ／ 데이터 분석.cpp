995970c0f6c2f788affaedd034d282fea8057e1f
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-04-17 10:21:10 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.66 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int sort_by_index;
+
+int getIndex(string key)
+{
+    const string keys[4] = { "code", "date", "maximum", "remain" };
+    int index;
+    for(index = 0; index < 4; index++)
+        if (keys[index] == key) break;
+    return index;
+}
+
+bool comp(vector<int> data1, vector<int> data2)
+{
+    if (data1[sort_by_index] < data2[sort_by_index])
+        return true;
+    return false;
+}
+
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by)
+{
+    vector<vector<int>> answer;
+    
+    // ext 값이 val_ext 보다 작은 데이터만 뽑기
+    int ext_index = getIndex(ext);
+    for(int i = 0; i < data.size(); i++)
+    {
+        if (data[i][ext_index] < val_ext)
+            answer.push_back(data[i]);
+    }
+    
+    // sort_by 값을 기준으로 오름차순 정렬하기
+    sort_by_index = getIndex(sort_by);
+    sort(answer.begin(), answer.end(), comp);
+    
+    return answer;
+}
