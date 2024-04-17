cd786609a0fd464e6283208750aee00cede1b9a7
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-04-18 08:58:19 +0900

    [level 2] Title: 요격 시스템, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define S 0
+#define E 1
+
+bool comp(vector<int> t1, vector<int> t2)
+{
+    if (t1[E] < t2[E])
+        return true;
+    return false;
+}
+
+#include <iostream>
+
+int solution(vector<vector<int>> targets)
+{
+    sort(targets.begin(), targets.end());
+    
+    vector<int> cur = { targets[0][S], targets[0][E] };
+    int answer = 1;
+    for(int i = 1; i < targets.size(); i++)
+    {
+        vector<int> tg = targets[i];
+        
+        if ((cur[S] < tg[S] && tg[S] < cur[E]) ||
+            tg[S] < cur[E] && cur[E] < tg[E])
+        {
+            cur[S] = max(cur[S], tg[S]);
+            cur[E] = min(cur[E], tg[E]);
+        }
+        else
+        {
+            answer += 1;
+            cur = tg;   
+        }
+    }
+    
+    return answer;
+}
