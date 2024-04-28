71dd5f1a3f7cd9c33853e9d09faa140953a9cd70
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-04-28 15:45:38 +0900

    [Gold IV] Title: 인구 이동, Time: 488 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,96 +1,79 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <memory.h>
 using namespace std;
 
-#define MAX 50
-
 int N, L, R;
-int A[MAX][MAX];
-bool visited[MAX][MAX];
+int population[50][50];
+bool visited[50][50] = {false};
 
-int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, -1, 1 };
 
-int BFS(int x, int y)
+bool BFS(int y, int x)
 {
-	vector<pair<int, int> > countries; // 연합을 이루는 나라들의 위치
-	countries.push_back(make_pair(x, y));
-	int total_p = A[x][y]; // 연합을 이루는 총 인구수
-
 	queue<pair<int, int> > Q;
+	vector<pair<int, int> > unit;
 	Q.push(make_pair(x, y));
-	visited[x][y] = true;
+	unit.push_back(make_pair(x, y));
+	int average = population[y][x];
+	visited[y][x] = true;
 
-	while(!Q.empty())
+	while (!Q.empty())
 	{
 		int cx = Q.front().first, cy = Q.front().second;
 		Q.pop();
-
-		for(int i=0; i<4; i++)
+		for(int i = 0; i < 4; i++)
 		{
 			int nx = cx + dx[i], ny = cy + dy[i];
-			if(nx < 0 || ny < 0 || nx >= N || ny >= N)
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N)
 				continue;
-			if(visited[nx][ny])
-				continue;
-			int p_diff = abs(A[cx][cy] - A[nx][ny]); // 인구 수 차이
-			if(p_diff < L || R < p_diff)
+			if (visited[ny][nx])
 				continue;
 
-			countries.push_back(make_pair(nx, ny));
-			total_p += A[nx][ny];
+			int diff = abs(population[cy][cx] - population[ny][nx]);
+			if (diff < L || R < diff)
+				continue;
 
-			visited[nx][ny] = true;
+			average += population[ny][nx];
 			Q.push(make_pair(nx, ny));
+			unit.push_back(make_pair(nx, ny));
+			visited[ny][nx] = true;
 		}
 	}
-	if(countries.size() == 1)
-		return -1;
 
-	int new_p = total_p / countries.size(); // 연합을 이루는 각 나라의 새로운 인구수
-	for(size_t i=0; i<countries.size(); i++)
-		A[countries[i].first][countries[i].second] = new_p;
-	return 0;
-}
-
-int populationMovement()
-{
-	int ret = -1;
-
-	for(int i=0; i<N; i++)
-		for(int j=0; j<N; j++)
-			visited[i][j] = false;
-	
-	for(int i=0; i<N; i++)
-	{
-		for(int j=0; j<N; j++)
-		{
-			if(visited[i][j])
-				continue;
-			if(BFS(i,j) == 0)
-				ret = 0;
-		}
-	}
-	return ret;
+	average /= unit.size();
+	for(size_t i = 0; i < unit.size(); i++)
+		population[unit[i].second][unit[i].first] = average;
+	return unit.size() > 1 ? true : false;
 }
 
 int main()
 {
 	cin >> N >> L >> R;
-	for(int i=0; i<N; i++)
-		for(int j=0; j<N; j++)
-			cin >> A[i][j];
+	for(int i = 0; i < N; i++)
+		for(int j = 0; j < N; j++)
+			cin >> population[i][j];
 
 	int day = 0;
-	while(1)
+	while (true)
 	{
-		if (populationMovement() == -1)
-			break;
+		memset(&visited, false, sizeof(visited));
+		bool move = false;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if (visited[i][j]) continue;
+				if (BFS(i, j) == true) // 연합이 이루어진 경우
+					move = true;
+			}
+		}
+		if (!move) break; // 연합이 하나도 이루어지지 않은 경우 (인구 이동 X)
 		day++;
 	}
-	cout << day << '\n';
 
+	cout << day << '\n';
 	return 0;
 }
