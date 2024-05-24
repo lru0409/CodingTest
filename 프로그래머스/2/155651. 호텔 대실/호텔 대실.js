1d60fd641efe19436245ab845e6c5d80d7974f47
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-05-24 09:16:32 +0900

    [level 2] Title: 호텔 대실, Time: 10.52 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -4,34 +4,31 @@ function solution(book_time) {
     const rooms = []; // 현재 사용 중인 룸들이 사용 가능해지는 시간
     
     const plusTenMin = function(time) {
-        const newTime = [ ...time ];
-        newTime[1] += 10;
-        if (newTime[1] >= 60) {
-            newTime[1] %= 60;
-            newTime[0] += 1;
-        }
-        return newTime;
+        let [newHour, newMinute] = time;
+        newHour += Math.floor((newMinute + 10) / 60);
+        newMinute = (newMinute + 10) % 60;
+        return [newHour, newMinute];
     }
     
     // 기존 룸으로 가능할 시 룸 인덱스, 불가능할 시 -1 반환
-    const checkRooms = function(time) {
+    const findAvailableRoom = function(checkInTime) {
         for(let i = 0; i < rooms.length; i++) {
-            if (rooms[i][0] < time[0] ||
-                (rooms[i][0] === time[0] && rooms[i][1] <= time[1]))
+            const [roomHour, roomMin] = rooms[i];
+            const [checkInHour, checkInMin] = checkInTime;
+            if (roomHour < checkInHour ||
+                roomHour === checkInHour && roomMin <= checkInMin)
                 return i;
         }
         return -1;
     }
     
     for(let time of book_time) {
-        time = time.map((timeStr) => { // ["시:분", "시,분"] => [[시,분][시,분]]
-            return timeStr.split(':').map((numStr) => parseInt(numStr));
-        })
-        let roomIndex = checkRooms(time[0]); // 임실 가능한 룸이 있는지 확인
+        const [checkIn, checkOut] = time.map((timeStr) => timeStr.split(':').map(Number));
+        let roomIndex = findAvailableRoom(checkIn); // 임실 가능한 룸이 있는지 확인
         if (roomIndex === -1) { // 입실 가능한 룸 없음 -> 새로운 룸 사용
-            rooms.push(plusTenMin(time[1]));
+            rooms.push(plusTenMin(checkOut));
         } else { // 입실 가능한 룸 있음 -> roomIndex의 룸 사용
-            rooms[roomIndex] = plusTenMin(time[1]);
+            rooms[roomIndex] = plusTenMin(checkOut);
         }
     }
     
