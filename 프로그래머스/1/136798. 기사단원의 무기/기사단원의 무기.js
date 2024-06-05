a460c202d871414d8d62166919cc9870f0f6d053
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-06-05 09:04:39 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.54 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,25 @@
+function solution(number, limit, power) {
+    
+    let answer = 0;
+    
+    for (let i = 1; i <= number; i++) {
+        let count = 0;
+        for (let j = 1; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                count += 1;
+                if (i / j !== j) count += 1;
+            }
+        }
+        if (limit < count)
+            answer += power;
+        else
+            answer += count;
+    }
+
+    return answer;
+}
+
+
+
+// 15
+// 
