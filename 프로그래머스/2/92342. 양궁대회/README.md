d4c9c9a88a8826d57da72a1b3ffdd497da3670b5
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-06-13 09:53:04 +0900

    [level 2] Title: 양궁대회, Time: 1.97 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#) 
 
 ### 성능 요약
 
-메모리: 37.5 MB, 시간: 2.26 ms
+메모리: 36.8 MB, 시간: 1.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 13일 09:33:39
+2024년 06월 13일 09:53:00
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -5,38 +5,47 @@ function solution(n, info) {
     
     const check = function(arrowCounts) {  
         let myScore = 0, opponentScore = 0;
-        let myTotalCount = 0;
         for (let i = 0; i < 11; i++) {
-            if (info[i] === 0 && arrowCounts[i] === 0)
-                continue;
-            if (info[i] < arrowCounts[i])
-                myScore += (10 - i);
-            else
-                opponentScore += (10 - i);
-            myTotalCount += arrowCounts[i];
+            if (info[i] === 0 && arrowCounts[i] === 0) continue;
+            if (info[i] < arrowCounts[i]) myScore += (10 - i);
+            else opponentScore += (10 - i);
         }
-        if (maxScoreDiff <= myScore - opponentScore) {
-            maxScoreDiff = myScore - opponentScore;
-            if (myTotalCount < n)
-                arrowCounts[10] = n - myTotalCount;
-            answer = arrowCounts;
+        
+        let scoreDiff = myScore - opponentScore;
+        if (maxScoreDiff < scoreDiff) {
+            maxScoreDiff = scoreDiff;
+            answer = arrowCounts.slice();
+        } else if (maxScoreDiff === scoreDiff) {
+            for (let i = 10; i >= 0; i--) {
+                if (arrowCounts[i] < answer[i])
+                    break;
+                if (arrowCounts[i] > answer[i]) {
+                    answer = arrowCounts.slice();
+                    break;
+                }
+            }
         }
     }
     
-    const DFS = function(shotArrowCounts, remainArrowCount) {
-        if (shotArrowCounts.length === 11) {
+    const DFS = function(index, remainArrowCount, shotArrowCounts) {
+        if (index === 11 || remainArrowCount === 0) {
+            if (remainArrowCount > 0) shotArrowCounts[10] += remainArrowCount;
             check(shotArrowCounts);
             return;
         }
-        const opponentArrowCount = info[shotArrowCounts.length];
-        if (opponentArrowCount < remainArrowCount) {
-            const newShotArrowCount = [...shotArrowCounts, opponentArrowCount + 1];
-            DFS(newShotArrowCount, remainArrowCount - opponentArrowCount - 1);
+        
+        // 이 점수 포기하기
+        DFS(index + 1, remainArrowCount, shotArrowCounts.slice());
+        
+        // 이 점수를 얻기 위해 필요한 화살 쏘기
+        if (info[index] < remainArrowCount) {
+            const newShotArrowCounts = shotArrowCounts.slice();
+            newShotArrowCounts[index] = info[index] + 1;
+            DFS(index + 1, remainArrowCount - newShotArrowCounts[index], newShotArrowCounts);
         }
-        DFS([...shotArrowCounts, 0], remainArrowCount);
     }
     
-    DFS([], n);
+    DFS(0, n, Array(11).fill(0));
     
     if (maxScoreDiff === 0)
         return [-1];
