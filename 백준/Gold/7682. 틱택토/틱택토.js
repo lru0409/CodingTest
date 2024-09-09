a91a2a1b81b300e5246da4dbe284661b06d74a73
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2024-09-10 08:15:49 +0900

    [Gold V] Title: 틱택토, Time: 116 ms, Memory: 9736 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.js
+++ 백준/Gold/7682. 틱택토/틱택토.js
@@ -30,60 +30,45 @@ function solution(cases) {
 			console.log("invalid");
 			return;
 		}
-		const oSuccess = [];
-		const xSuccess = [];
+		let oSuccess = false;
+		let xSuccess = false;
 		for (let y = 0; y < 3; y++) {
 			if (board[y][0] == board[y][1] && board[y][1] == board[y][2]) {
-				if (board[y][0] == 'O') oSuccess.push([[y, 0], [y, 1], [y, 2]]);
-				else if (board[y][0] == 'X') xSuccess.push([[y, 0], [y, 1], [y, 2]]);
+				if (board[y][0] == 'O') oSuccess = true;
+				else if (board[y][0] == 'X') xSuccess = true;
 			}
 		}
 		for (let x = 0; x < 3; x++) {
 			if (board[0][x] == board[1][x] && board[1][x] == board[2][x]) {
-				if (board[0][x] == 'O') oSuccess.push([[0, x], [1, x], [2, x]]);
-				else if (board[0][x] == 'X') xSuccess.push([[0, x], [1, x], [2, x]]);
+				if (board[0][x] == 'O') oSuccess = true;
+				else if (board[0][x] == 'X') xSuccess = true;
 			}
 		}
 		if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-			if (board[0][0] == 'O') oSuccess.push([[0, 0], [1, 1], [2, 2]]);
-			else if (board[0][0] == 'X') xSuccess.push([[0, 0], [1, 1], [2, 2]]);
+			if (board[0][0] == 'O') oSuccess = true;
+			else if (board[0][0] == 'X') xSuccess = true;
 		}
 		if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-			if (board[0][2] == 'O') oSuccess.push([[0, 2], [1, 1], [2, 0]]);
-			else if (board[0][2] == 'X') xSuccess.push([[0, 2], [1, 1], [2, 0]]);
+			if (board[0][2] == 'O') oSuccess = true;
+			else if (board[0][2] == 'X') xSuccess = true;
 		}
 		// 둘 다 성공했으면 안 된다.
-		if (oSuccess.length && xSuccess.length) {
+		if (oSuccess && xSuccess) {
 			console.log("invalid");
 			return;
 		}
-		// X는 두 개 이상 성공할 수 있다. 다만 두 줄에서 중복되는 위치가 있어야 한다.
-		if (xSuccess.length == 2) {
-			const duplicated = xSuccess[0].find((pos1) => {
-				let duplicated = false;
-				xSuccess[1].forEach((pos2) => {
-					if (pos1[0] == pos2[0] && pos1[1] == pos2[1])
-						duplicated = true;
-				});
-				return duplicated;
-			});
-			if (!duplicated) {
-				console.log("invalid");
-				return;
-			}
-		}
 		// O가 이긴 경우, X와 O의 개수가 같아야 한다.
-		if (oSuccess.length && oCount != xCount) {
+		if (oSuccess && oCount != xCount) {
 			console.log("invalid");
 			return;
 		}
 		// X가 이긴 경우, X가 O보다 1개 더 많아야 한다.
-		if (xSuccess.length && oCount + 1 != xCount) {
+		if (xSuccess && oCount + 1 != xCount) {
 			console.log("invalid");
 			return;
 		}
 		// 최종 상태(누군가 이겼거나, 판이 다 참)여야 한다.
-		if (!oSuccess.length && !xSuccess.length && oCount + xCount != 9) {
+		if (!oSuccess && !xSuccess && oCount + xCount != 9) {
 			console.log("invalid");
 			return;
 		}
