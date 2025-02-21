13b19c92b2d1ca2b3b8a142d6185ec000ed84759
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2025-02-21 10:36:58 +0900

    [Silver I] Title: 팔, Time: 124 ms, Memory: 9496 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.js
+++ 백준/Silver/1105. 팔/팔.js
@@ -19,12 +19,8 @@ const solution = (L, R) => {
     }
     let count = 0;
     for (let i = 0; i < L.length; i++) {
-        if (L[i] !== "8" && L[i] === R[i]) continue;
-        if (L[i] !== "8" || R[i] !== "8") break;
-        count++;
+        if (L[i] !== R[i]) break;
+        if (L[i] === "8" && R[i] === "8") count++;
     }
     console.log(count);
 }
-
-// 두 수의 자릿수가 다를 경우 => 0
-// 앞자리부터 둘 다 8인 자릿수까지 이동하며 카운트 세기
