106ce0009e0eb9fb45fc139432df4ba3f0ee56a5
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2025-03-24 15:58:14 +0900

    [level 2] Title: 모음 사전, Time: 3.03 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,30 +1,19 @@
 function solution(word) {
-    const index = { // 알파벳 별 인덱스
-        "A": 0, "E": 1, "I": 2, "O": 3, "U": 4
-    }
-    let answer = 0;
-    let totalCount = 0;
-    for (let i = 0; i < word.length; i++) {
-        const alphabet = word[i];
-        let count = 1; // i 인덱스로 한 알파벳이 들어올 때의 모든 경우의 수 개수를 구할 예정
-        for (let j = 1; j < 5 - i; j++) {
-            count += Math.pow(5, j);
+    const alphabets = ['A', 'E', 'I', 'O', 'U'];
+    let count = 0;
+    
+    const permutation = (p) => {
+        if (p.join("") === word) return count;
+        if (p.length === 5) return undefined;
+        for (let i = 0; i < 5; i++) {
+            p.push(alphabets[i]);
+            count++;
+            const ret = permutation(p);
+            if (ret !== undefined) return ret;
+            p.pop();
         }
-        totalCount += index[alphabet] * count + 1; // i 인덱스로 alphabet 이전의 문자가 왔을 때, 모든 경우의 수 개수 + 1 = 현재 개수
+        return undefined;
     }
-    return totalCount;
+    
+    return permutation([]);
 }
-
-// 5P1 => 5
-// 5P2 => 25
-// 5P3 => 125
-// 5P4 => 625
-// 5P5 => 3125
-
-// 첫 글자 인덱스 * 781(1 + 5P1 + 5P2 + 5P3 + 5P4) + 1
-// 두 글자 인덱스 * 156(1 + 5P1 + 5P2 + 5P3) + 1
-// 세 글자 인덱스 * 31(1 + 5P1 + 5P2) + 1
-// 네 글자 인덱스 * 6(1 + 5P1) + 1
-// 다섯 글자 인덱스 + 1
-
-
